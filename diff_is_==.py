85488ec4a33d467b320186425d97be2c09b53ea8
Digvijay Thavare <digvijaythavare31@gmail.com>
2026-05-28 15:42:48 +0530

    memoryobject

---
+++ diff_is_==.py
@@ -0,0 +1,14 @@
+a = [10,20]
+b = [10,20]
+
+if a == b:
+    print("a is equal to b")
+else:
+    print("a is not equal to b")
+
+if a[0] is b[0]:
+    print("a is the same object as b")
+else:
+    print("a is not the same object as b")    
+
+
