40fd895fda8006185643b676eb4a2ea06bc818cc
Digvijay Thavare <digvijaythavare31@gmail.com>
2026-06-22 07:36:39 +0530

    Class blueprnt

--- blueprint.py
+++ blueprint.py
@@ -1,5 +1,5 @@
 class Person:
-    def __init__(self,name,age,city,country,amount=10):
+    def __init__(self,name,age,city,country,amount):
         self.name = name
         self.age = age
         self.city = city
